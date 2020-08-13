5746f97dc2b06967ee99d83cfcdd7f2f473f15d0
Darnycya Smith <darnycyasmith@DarnycymithsMBP.lan1>
2020-08-13 12:10:06 -0400

    Log out created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './components/Header/Header'
 import Main from './components/Main'
+import { verifyUser } from './services/auth'
 import './App.css';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    handleVerify()
+  }, [])
+
+  const handleVerify = async () => {
+    const userData = await verifyUser()
+    setCurrentUser(userData);
+  }
 
 
 
@@ -12,6 +22,7 @@ function App() {
     <div className="App">
       <Header
         currentUser={currentUser}
+        setCurrentUser={setCurrentUser}
       />
       <Main
         setCurrentUser={setCurrentUser}/>
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
+import { removeToken } from '../../services/auth';
+import { useHistory } from 'react-router-dom'
 
 export default function Header(props) {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    props.setCurrentUser(null);
+    localStorage.removeItem("authToken");
+    removeToken();
+    history.push('/')
+
+  }
+
+
   return (
     <header>
       <h1>Escape NYC</h1>
@@ -10,7 +23,7 @@ export default function Header(props) {
         props.currentUser ? (
           <>
             <img src={props.currentUser.user_image}/>
-            <button>Logout</button>
+            <button onClick={handleLogout}>Logout</button>
           </>
         ) : (
             <Link to='/login'>Login/Register</Link>
