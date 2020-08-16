a240f3aa9de2ce3bfdf98fb02b9eccfca36e1d38
Darnycya Smith <darnycyasmith@DarnycymithsMBP.lan1>
2020-08-16 18:43:53 -0400

    updated header

--- client/src/components/TrailDetail/TrailDetail.jsx
+++ client/src/components/TrailDetail/TrailDetail.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { readOneTrail } from '../../services/trails'
+import Comments from '../Comments/Comments'
+import './TrailDetail.css'
 
 
 
@@ -22,28 +24,26 @@ export default function TrailDetail(props) {
     <div class="details">
       {trail && ( 
         <>
-        <h3>{trail.name}</h3>
+        <h4>{trail.name}</h4>
         <img class="trail_image_big" src={trail.trail_image}></img>
           <p>{trail.name}</p>
           <p>{trail.difficulty}</p>
           <p>{trail.travel_time_from_NYC} minutes</p>
           </>
-      )}
+      )} 
       <button>Update Trail</button> 
       <button>Delete Trail</button> 
-      <h3>Comments:</h3>
-      {props.comments.map((comment) => (
-        <div class="comments">
-          
-          <img class="user_image" src={comment.user_id.user_image}></img>
-          <p>{comment.user_id.name}</p>
-            <p>{comment.comment}</p>
-        </div>
-      ))
-      }
-      <input type="text"></input><button>Add A Comment </button>
-      
     </div>
     
   )
 }
+
+// {trail && ( 
+//   <>
+//   <h3>{trail.name}</h3>
+//   <img class="trail_image_big" src={trail.trail_image}></img>
+//     <p>{trail.name}</p>
+//     <p>{trail.difficulty}</p>
+//     <p>{trail.travel_time_from_NYC} minutes</p>
+//     </>
+// )}
