b52bfe38c8b927f1fe5ba93a7490e64dfd3fae86
Darnycya Smith <darnycyasmith@DarnycymithsMBP.lan1>
2020-08-19 17:09:38 -0400

    argumentatively

--- client/src/screens/HomePage/HomePage.jsx
+++ client/src/screens/HomePage/HomePage.jsx
@@ -9,7 +9,7 @@ export default function HomePage() {
     <body className="body-homepage">
     <div className="home-wrap">
       <h3 class="glow">Escape NYC</h3>
-      <div class="info"><h2>The concrete jungle, NYC, is argumentavly the greatest city on Earth. But the grass is greener and more prominent on the other side...of the bridges. Mountains and trails await you and Escape NYC will help guide you to the perfect one for you.</h2></div>
+      <div class="info"><h2>The concrete jungle, NYC, is argumentatively the greatest city on Earth. But the grass is greener and more prominent on the other side...of the bridges. Mountains and trails await you and Escape NYC will help guide you to the perfect one for you.</h2></div>
        
       
       <Link to='/login'><button class ="enter"><p class="enter-button">Enter</p></button></Link>
