476b14b6fd54c40e8118acc248a5a00725641149
Darnycya <Darnycya@gmail.com>
2021-08-05 20:32:27 -0400

    changed background and div colors, adjusted height of divs, header and pictures

--- src/Screens/LogIn/LogIn.jsx
+++ src/Screens/LogIn/LogIn.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Form';
 import './LogIn.css';
 
 export default function Login() {
   return (
     <div>
-      <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+      <form action="/action_page.php" method="post">
+      <input type="text" name="fname" required></input>
+      <input type="submit" value="Submit"></input>
+      </form>
     </div>
   )
 }
+
