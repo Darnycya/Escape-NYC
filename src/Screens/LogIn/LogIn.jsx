9c7f6c1393976dfbff74d5e21a7a58ca42ff671e
Darnycya <Darnycya@gmail.com>
2021-08-05 21:34:20 -0400

    created login and sign in form

--- src/Screens/LogIn/LogIn.jsx
+++ src/Screens/LogIn/LogIn.jsx
@@ -3,12 +3,27 @@ import './LogIn.css';
 
 export default function Login() {
   return (
-    <div>
-      <form action="/action_page.php" method="post">
-      <input type="text" name="fname" required></input>
-      <input type="submit" value="Submit"></input>
-      </form>
-    </div>
+    <div className="form-container">
+      <form className="login-form">
+      Log In:<br/>
+          <input placeholder="Username"></input><br/>
+          <input placeholder="Password"></input><br />
+        <button>Log In</button><br/>
+        Forgot your password?<br />
+        Forgot your username?<br/>
+        </form>
+      <form className="signup-form">
+      Sign Up:<br/>
+          <input placeholder="First Name"></input>
+          <input placeholder="Last Name"></input><br />
+          <input placeholder="Username"></input><br />
+          Username can only contain letters and numbers.<br />
+          <input placeholder="Password"></input>
+          <input placeholder="Confirm Password"></input><br />
+          Password should contain more than 6 letters.<br />
+          <button>Submit</button>
+        </form>
+     </div> 
   )
 }
 
