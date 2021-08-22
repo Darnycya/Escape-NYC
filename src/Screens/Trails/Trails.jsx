050d04db487243d2bb1d2815786dc134d567aa5a
Darnycya <Darnycya@gmail.com>
2021-08-22 19:55:53 -0400

    fixed trails

--- src/Screens/Trails/Trails.jsx
+++ src/Screens/Trails/Trails.jsx
@@ -8,11 +8,11 @@ export default function Trails() {
     <div className="top-container">
         <div className="search-container">
         <input className="searchbar" placeholder="Search"></input><button className="search-button">Go</button>
-        </div> 
-        <div className="filter-container">
+        </div>
+        {/* <div className="filter-container">
           Filter:
           <div className="filters">
-          Level:<br/>
+          <div className="filter-labels">Level:</div>
         <input className="filter" type="checkbox" id="Easy" name="Easy"
            />
         <label for="scales">Easy</label><br/>
@@ -24,7 +24,7 @@ export default function Trails() {
             <label for="scales">Hard</label><br />
           </div>
           <div className="filters">
-          Distance:<br/>
+          <div className="filter-labels">Distance:</div>
         <input className="filter" type="checkbox" id="0-25" name="0-25"
            />
         <label for="scales">0-25</label><br/>
@@ -39,7 +39,7 @@ export default function Trails() {
           <label for="scales">100+</label><br />
         </div>
         <div className="filters">
-          Rating:<br/>
+        <div className="filter-labels">Rating:</div>
         <input className="filter" type="checkbox" id="0-25" name="0-25"
          un  />
         <label for="scales">0-25</label><br/>
@@ -53,20 +53,24 @@ export default function Trails() {
          />
           <label for="scales">100+</label><br />
       </div>
-      </div>
+      </div> */}
       </div>
 <div className="trails">
       
         <div className="trail">
           <div className="trail-image-container">
-            <img className="trail-image" alt="" src="https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png"></img>
+            <img className="trail-image" alt="" src="https://hikethehudsonvalley.com/wp-content/uploads/2015/05/Spring2014_RWAN_955.jpg"></img>
           </div>
           <div className="trail-description-container">
-            Name:<br />
-            Level:<br />
-            Distance:<br />
-            Rating:<br />
-            Username<br/>
+            <h6><b>Name:</b> Anthony's Nose</h6>
+            <h6><b>Level:</b> Medium</h6>
+            <h6><b>Distance From NYC:</b> 40 miles</h6>
+            <h6><b>Length:</b> 2.6 miles roundtrip</h6>
+            <h6><b>Time To Climb:</b> 2.5 hours</h6>
+            <h6><b>Rating:</b>  <img alt="star" className="star" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWNnGdm_9JNZzTDOKVhALeT_bw6Pvh5nZFraKlT4njFdpjul7HmqchP1uyRy6NGyyDq8I&usqp=CAU"></img>
+              <img alt="star" className="star" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWNnGdm_9JNZzTDOKVhALeT_bw6Pvh5nZFraKlT4njFdpjul7HmqchP1uyRy6NGyyDq8I&usqp=CAU"></img>
+              <img alt="star" className="star" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWNnGdm_9JNZzTDOKVhALeT_bw6Pvh5nZFraKlT4njFdpjul7HmqchP1uyRy6NGyyDq8I&usqp=CAU"></img>
+              <img alt="star" className="star" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWNnGdm_9JNZzTDOKVhALeT_bw6Pvh5nZFraKlT4njFdpjul7HmqchP1uyRy6NGyyDq8I&usqp=CAU"></img></h6>
             </div>
       </div>
       
