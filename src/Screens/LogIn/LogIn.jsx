7a539b2bc449e5295fc3a8d73e937a8876a68291
Darnycya <Darnycya@gmail.com>
2021-08-22 18:12:10 -0400

    login page css complete

--- src/Screens/LogIn/LogIn.jsx
+++ src/Screens/LogIn/LogIn.jsx
@@ -5,23 +5,23 @@ export default function Login() {
   return (
     <div className="form-container">
       <form className="login-form">
-      Log In:<br/>
+      <p className="fake-labels"><b>Log In:</b></p><br/>
           <input className="login" placeholder="Username"></input><br/>
           <input className="login" placeholder="Password"></input><br />
-        <button>Log In</button><br/>
-        Forgot your password?<br />
-        Forgot your username?<br/>
+        <button className="login-button">Log In</button><br/>
+        <p className="fake-labels">Forgot your password?</p><br />
+        <p className="fake-labels">Forgot your username?</p><br/>
         </form>
       <form className="signup-form">
-      Sign Up:<br/>
+      <p className="fake-labels"><b>Sign Up:</b></p><br/>
           <input className="signup" placeholder="First Name"></input>
           <input className="signup" placeholder="Last Name"></input><br />
           <input className="signup" placeholder="Username"></input><br />
-          Username can only contain letters and numbers.<br />
+          <p className="fake-labels">Username can only contain letters and numbers.</p><br />
           <input className="signup" placeholder="Password"></input>
           <input className="signup" placeholder="Confirm Password"></input><br />
-          Password should contain more than 6 letters.<br />
-          <button>Submit</button>
+          <p className="fake-labels">Password should contain more than 6 letters.</p><br />
+          <button className="submit-button">Submit</button>
         </form>
      </div> 
   )
