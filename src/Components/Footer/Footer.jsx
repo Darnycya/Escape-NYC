6c51d96ccc3bd4b6d2b250c82a9e14685bb0f921
Darnycya <Darnycya@gmail.com>
2021-11-04 12:14:44 -0400

    added links to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ export default function Footer() {
     <div className="footer">
 
       <div className="footer-icon-div">
-        <img className="footer-icon" alt="LinkedIn" src="https://image.flaticon.com/icons/png/512/174/174857.png"></img>
-        <img className="footer-icon" alt="Medium" src="https://cdn.iconscout.com/icon/free/png-512/medium-47-433328.png"></img>
-      <img className="footer-icon" alt="Darnycya's Website" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpl181MMMyVZeKUOHiYQi3ZTKgPoCFpx-LSg&usqp=CAU"></img>
+        <a href="https://www.linkedin.com/in/darnycya/"><img className="footer-icon" alt="LinkedIn" src="https://image.flaticon.com/icons/png/512/174/174857.png"></img></a>
+        <a href="https://medium.com/@darnycya"><img className="footer-icon" alt="Medium" src="https://cdn.iconscout.com/icon/free/png-512/medium-47-433328.png"></img></a>
+      <a href="https://www.darnycya.com/"><img className="footer-icon" alt="Darnycya's Website" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpl181MMMyVZeKUOHiYQi3ZTKgPoCFpx-LSg&usqp=CAU"></img></a>
       </div>
       </div>
   )
