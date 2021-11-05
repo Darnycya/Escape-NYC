57098e3f6aaa250b90adac1e125b01e66ba0b6eb
Darnycya <Darnycya@gmail.com>
2021-11-04 21:21:21 -0400

    added Loading text

--- src/Screens/Trails/Trails.jsx
+++ src/Screens/Trails/Trails.jsx
@@ -24,7 +24,8 @@ const Trails = (props) => {
   }, [])
 
   if (!isLoaded) {
-    return <img alt="spinner" className="spinner" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a36e39ed-2182-4fa3-af43-299e3d89d2de/db8jbxg-51f41c0c-b7ed-48f6-9128-3e5e701bfd88.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2EzNmUzOWVkLTIxODItNGZhMy1hZjQzLTI5OWUzZDg5ZDJkZVwvZGI4amJ4Zy01MWY0MWMwYy1iN2VkLTQ4ZjYtOTEyOC0zZTVlNzAxYmZkODguZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.XDGFAM5d5ZD20e_6JEQQrAkqOFwIwIhaTsxn4Uymnxk"></img>
+    return <div className="loading-div"><img alt="spinner" className="spinner" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a36e39ed-2182-4fa3-af43-299e3d89d2de/db8jbxg-51f41c0c-b7ed-48f6-9128-3e5e701bfd88.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2EzNmUzOWVkLTIxODItNGZhMy1hZjQzLTI5OWUzZDg5ZDJkZVwvZGI4amJ4Zy01MWY0MWMwYy1iN2VkLTQ4ZjYtOTEyOC0zZTVlNzAxYmZkODguZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.XDGFAM5d5ZD20e_6JEQQrAkqOFwIwIhaTsxn4Uymnxk"></img>
+    <h1 className="loading-text">Loading...</h1></div>
 }
 
   const handleSearch = event => {
