fa93dc7676b2fc1bcb6f38445bf7fab02791690a
Darnycya <Darnycya@gmail.com>
2021-11-09 15:59:58 -0500

    header is responsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,11 +8,8 @@ export default function Layout() {
       <Link className="escape-logo" to="/"><img className="logo" alt="Logo" src="https://res.cloudinary.com/darnycya/image/upload/v1620845608/EscapeNYClogo_cppudw.jpg"></img></Link>
       
       <div className="nav-links">
-        <Link to="/trails" className="link">Trails</Link>
-      </div>
-      
-      <div className="nav-login">
-      <Link to="/login-in" className="signIn-link">Login In | Sign Up</Link>
+        <Link to="/trails" className="trails-link">Trails</Link>
+        <Link to="/login-in" className="signIn-link">Login In | Sign Up</Link>
       </div>
 
     </div>
